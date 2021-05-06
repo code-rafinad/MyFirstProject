096856e228ea3b0aefc3b274c27475b754c0c66e
Rafinad <rafinad@outlook.com>
2021-05-06 22:57:22 +0300

    Initial Commit

---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Сахар Кубический on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
