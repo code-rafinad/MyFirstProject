96c20254c7940941817423d73dc7bf6c164428d0
Rafinad <rafinad@outlook.com>
2021-05-07 02:33:32 +0300

    teach about sliders

--- MyFirstProject/ContentView.swift
+++ MyFirstProject/ContentView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showHello = true
+    @State private var name = ""
+    @State private var password = ""
+    @State private var showDetail = false
+    @State private var celcius: Double = 0
+    var textButton = 1
     var body: some View {
         VStack{
-            Toggle(isOn: $showHello, label: {
-                Text("Label")
-            })
-            .toggleStyle(SwitchToggleStyle(tint: Color.yellow))
-       // .padding()
-            if showHello {
-                Text("Helloef")
-                    .foregroundColor(.blue)
-            }
+            Slider(value: $celcius, in: -100...100, step: 0.1)
+            
+            TextField("Enter your name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            SecureField("Enter your password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Hello, \(name), your password is \(password), temperature is \(celcius) celcius, that \(celcius * 9/5 + 32) Farenheit")
         }
     }
 }
