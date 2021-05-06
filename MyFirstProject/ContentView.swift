e813cfe5ea288878990b95c7f31a1511bb1c3581
Rafinad <rafinad@outlook.com>
2021-05-07 00:23:36 +0300

    gradient exercise

--- MyFirstProject/ContentView.swift
+++ MyFirstProject/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, swift!")
-            .padding()
+        let colors = Gradient(colors: [.red, .yellow, .blue])
+        let gradient = AngularGradient(gradient: colors, center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        return Circle()
+            .strokeBorder(gradient, lineWidth: 50)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
