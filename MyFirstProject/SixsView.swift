3e7784e98fcd88c85ed273769cdbc13a45978954
Rafinad <rafinad@outlook.com>
2021-05-07 10:58:41 +0300

    Sections in List

---
+++ MyFirstProject/SixsView.swift
@@ -0,0 +1,42 @@
+//
+//  SixsView.swift
+//  MyFirstProject
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+
+struct TaskRow: View {
+    var body: some View {
+        Text("Some Task")
+    }
+}
+
+struct SixsView: View {
+    var body: some View {
+        List{
+            Section(header: Text("Important Tasks")) {
+                TaskRow()
+                TaskRow()
+                TaskRow()
+                TaskRow()
+                
+            }
+            Section(header: Text("Other Tasks")) {
+                TaskRow()
+                TaskRow()
+                TaskRow()
+                TaskRow()
+                
+            }
+        }
+    }
+}
+
+struct SixsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SixsView()
+    }
+}
